1454610ffd812e996b095cf052cc193f7b91f304
liviaholties <38703934+liviaholties@users.noreply.github.com>
2018-06-28 14:17:14 +0200

    tekening

---
+++ tekening.js
@@ -0,0 +1,14 @@
+
+
+function setup() {
+  //Canvas van 200 px bij 200 px
+  createCanvas(2000, 2000); 
+  //Één cijfer geeft grijswaarden
+background(220,0,0); }
+
+function draw () {
+	fill(153, 204, 255)
+	rect(100, 100, 300, 200)
+}
+  
+  
